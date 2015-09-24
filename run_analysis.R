fa9a13bd287f7082a44ff9dfb3df427754f641f1
christopher floess <skeptikos@gmail.com>
2015-09-24 14:04:13 +0200

    tidy up source code and output complete dataset to tidy.txt

--- run_analysis.R
+++ run_analysis.R
@@ -1,33 +1,41 @@
 library(reshape2)
 library(dplyr)
 
-# Column names for test data
+# First we process the "test" data
+
+# Column names for data
 features <- read.table('UCI HAR Dataset/features.txt')
+
+# Labels for the numerical activity identifiers
+activitiesLabels <- read.table('UCI HAR Dataset/activity_labels.txt', col.names = c('index', 'label'))
 
 # Read "test" data and set column names
 testData <- read.table('UCI HAR Dataset/test/X_test.txt', col.names = features[,2])
-trainData <- read.table('UCI HAR Dataset/train/X_train.txt', col.names = features[,2])
 
 # Discard columns that do not have mean or std in them.
 reducedTestData <- testData[,grep('mean|std', colnames(testData), value = T, ignore.case = T)]
-reducedTrainData <- trainData[,grep('mean|std', colnames(trainData), value = T, ignore.case = T)]
 
 # Numerical activity identifiers
 testActivities <- read.table('UCI HAR Dataset/test/y_test.txt', col.names = c('activityIndex'), colClasses = c('factor'))
-trainActivities <- read.table('UCI HAR Dataset/train/y_train.txt', col.names = c('activityIndex'), colClasses = c('factor'))
 
-# The actual activity labels
-activitiesLabels <- read.table('UCI HAR Dataset/activity_labels.txt', col.names = c('index', 'label'))
-# Set labels for reducedTestData$activityLabels
+# Set labels for activity identifiers
 levels(testActivities$activityIndex) <- activitiesLabels$label
-levels(trainActivities$activityIndex) <- activitiesLabels$label
 
-# Add subjects
+# Test subjects
 testSubjects <- read.table('UCI HAR Dataset/test/subject_test.txt')
-## reducedTestData$subjects <- subjects$V1
-trainSubjects <- read.table('UCI HAR Dataset/train/subject_train.txt')
-## reducedTrainData$subjects <- subjects$V1
 
-# Aggregate columns by
+# Aggregate columns by subjects and activity
 aggTestData <- aggregate(reducedTestData, by = list(subjects = testSubjects$V1, labels <- testActivities$activityIndex), mean, na.action=na.pass, na.rm=T)
+
+# Now the same thing for the train data
+trainData <- read.table('UCI HAR Dataset/train/X_train.txt', col.names = features[,2])
+reducedTrainData <- trainData[,grep('mean|std', colnames(trainData), value = T, ignore.case = T)]
+trainActivities <- read.table('UCI HAR Dataset/train/y_train.txt', col.names = c('activityIndex'), colClasses = c('factor'))
+levels(trainActivities$activityIndex) <- activitiesLabels$label
+trainSubjects <- read.table('UCI HAR Dataset/train/subject_train.txt')
+
 aggTrainData <- aggregate(reducedTrainData, by = list(subjects = trainSubjects$V1, labels <- trainActivities$activityIndex), mean, na.action=na.pass, na.rm=T)
+
+# Finally we combine testData and trainData
+combinedData <- rbind(aggTestData, aggTrainData)
+write.table(combinedData, file = 'tidy.txt', row.names = F)
