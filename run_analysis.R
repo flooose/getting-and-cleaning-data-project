97aba401e84ad6a1057cdf1f1a619ab536eb8f1e
christopher floess <skeptikos@gmail.com>
2015-09-24 13:42:34 +0200

    rough draft for compiling test and training data

---
+++ run_analysis.R
@@ -0,0 +1,38 @@
+library(reshape2)
+library(dplyr)
+
+# Column names for test data
+features <- read.table('UCI HAR Dataset/features.txt')
+
+# Read "test" data and set column names
+testData <- read.table('UCI HAR Dataset/test/X_test.txt', col.names = features[,2])
+trainData <- read.table('UCI HAR Dataset/train/X_train.txt', col.names = features[,2])
+
+# Discard columns that do not have mean or std in them.
+reducedTestData <- testData[,grep('mean|std', colnames(testData), value = T, ignore.case = T)]
+reducedTrainData <- trainData[,grep('mean|std', colnames(trainData), value = T, ignore.case = T)]
+
+# Numerical activity identifiers
+testActivities <- read.table('UCI HAR Dataset/test/y_test.txt', col.names = c('activityIndex'), colClasses = c('factor'))
+trainActivities <- read.table('UCI HAR Dataset/train/y_train.txt', col.names = c('activityIndex'), colClasses = c('factor'))
+# Add factor column for activities to reducedTestData
+## reducedTestData$activityLabels <- as.factor(testActivities$activityIndex)
+## reducedTrainData$activityLabels <- as.factor(trainActivities$activityIndex)
+
+# The actual activity labels
+activitiesLabels <- read.table('UCI HAR Dataset/activity_labels.txt', col.names = c('index', 'label'))
+# Set labels for reducedTestData$activityLabels
+levels(testActivities$activityIndex) <- activitiesLabels$label
+levels(trainActivities$activityIndex) <- activitiesLabels$label
+## levels(reducedTestData$activityLabels) <- activitiesLabels$label
+## levels(reducedTrainData$activityLabels) <- activitiesLabels$label
+
+# Add subjects
+testSubjects <- read.table('UCI HAR Dataset/test/subject_test.txt')
+## reducedTestData$subjects <- subjects$V1
+trainSubjects <- read.table('UCI HAR Dataset/train/subject_train.txt')
+## reducedTrainData$subjects <- subjects$V1
+
+# Aggregate columns by
+aggTestData <- aggregate(reducedTestData, by = list(subjects = testSubjects$V1, labels <- testActivities$activityIndex), mean, na.action=na.pass, na.rm=T)
+aggTrainData <- aggregate(reducedTrainData, by = list(subjects = trainSubjects$V1, labels <- trainActivities$activityIndex), mean, na.action=na.pass, na.rm=T)
